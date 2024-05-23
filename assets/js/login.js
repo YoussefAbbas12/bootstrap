8e207d270a7225182142b2406e5fb4cd2b2d82ed
YoussefAbbas12 <156310314+YoussefAbbas12@users.noreply.github.com>
2024-05-23 19:20:46 +0300

    Add files via upload

---
+++ assets/js/login.js
@@ -0,0 +1,6 @@
+function change(type, price1, quntity1){
+	type;
+	let model = document.querySelector('.model').value = type;
+	let price = document.querySelector('.sale').value = price1;
+	let quntity = document.querySelector('.quntity').value = quntity1;
+}
